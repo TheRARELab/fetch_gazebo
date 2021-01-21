6203c9900b12f8a83e001fa996360d30c068c2a1
Gregory LeMasurier <gregorylemasurier@gmail.com>
2021-01-20 21:27:55 -0500

    Fixed schunk machine table gears

--- fetchit_challenge/scripts/spawn_assembly_delayed_simple.sh
+++ fetchit_challenge/scripts/spawn_assembly_delayed_simple.sh
@@ -5,4 +5,11 @@ rosservice call /gazebo/get_physics_properties "{}"
 sleep 20
 
 echo "Spawning Assembly..."
-roslaunch fetchit_challenge assembly_spawn_simple.launch &
+roslaunch fetchit_challenge assembly_spawn_simple_arena_montreal2019.launch &
+
+#echo "Waiting for simulations to be ready"
+#rosservice call /gazebo/get_physics_properties "{}"
+#sleep 20
+
+#echo "Spawning Assembly..."
+#roslaunch fetchit_challenge assembly_spawn_simple.launch &
